b08876167ac48141bbcfc393c23f18bd9b9cf359
Irina Pilipenko <deineha94@gmail.com>
2019-12-18 16:05:06 +0300

    Add Form Html

--- app/Block.php
+++ app/Block.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Block extends Model
 {
-    //
+    protected $primaryKey='id';
+    protected $table ='blocks';
+    protected $fillable = array('title', 'topicid', 'content', 'imagePath',
+                                    'created_at', 'update_at');
 }
